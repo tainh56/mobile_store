a7c2e89c0d4275837ae959b7668c260c1283dc5f
Nguyen Huu Tai <nguyenhuutai1443@gmail.com>
2015-03-26 15:38:08 +0700

    Xoa mat hang

--- admin/XoaMatHang.php
+++ admin/XoaMatHang.php
@@ -1,8 +1,8 @@
+<?php include '../includes/connect-database.php'; ?>
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$id = $_GET['id'];
+echo "$id";
+$sql = "delete from mat_hang where id_mat_hang = $id";
+mysql_query($sql);
+?>
